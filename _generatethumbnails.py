2f9c2e2d531944060586aae15b2e1698ee469b22
Jeff Verkoeyen <jverkoey@gmail.com>
2013-12-01 10:15:11 -0600

    Generate thumbnails.

--- _generatethumbnails.py
+++ _generatethumbnails.py
@@ -1,4 +1,5 @@
 import Image
+import ImageOps
 import yaml
 import os
 
@@ -9,18 +10,22 @@ dropbox_photos_path = os.path.join(config['dayonepath'], 'photos/')
 large_path = "_site/gfx/dayone_large"
 thumb_path = "_site/gfx/dayone_thumb"
 
-os.makedirs(large_path)
-os.makedirs(thumb_path)
+if not os.path.exists(large_path):
+  os.makedirs(large_path)
+if not os.path.exists(thumb_path):
+  os.makedirs(thumb_path)
 
 for photo_path in os.listdir(dropbox_photos_path):
   full_photo_path = os.path.join(dropbox_photos_path, photo_path)
   if os.path.isfile(full_photo_path):
     img = Image.open(full_photo_path)
-    img.thumbnail((938,938), Image.ANTIALIAS)
-    img.save(os.path.join(large_path, photo_path))
+
+    thumb = ImageOps.fit(img, (100,100), Image.ANTIALIAS)
+    thumb.save(os.path.join(thumb_path, photo_path))
     
-    # TODO: Use ImageOps to create square thumbnails.
-    # http://stackoverflow.com/questions/1386352/pil-thumbnail-and-end-up-with-a-square-image
-    img = Image.open(full_photo_path)
-    img.thumbnail((100,100), Image.ANTIALIAS)
-    img.save(os.path.join(thumb_path, photo_path))
+    if img.size[0] > img.size[1] * 4 or img.size[0] * 4 > img.size[1]:
+      img.thumbnail((3000,3000), Image.ANTIALIAS)
+      img.save(os.path.join(large_path, photo_path))
+    else:
+      img.thumbnail((938,938), Image.ANTIALIAS)
+      img.save(os.path.join(large_path, photo_path))
