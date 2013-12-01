bb3927b0061ec14692cd340d6ee53951ba27a90d
Jeff Verkoeyen <jverkoey@gmail.com>
2013-12-01 14:42:15 -0600

    Update genthumb script.

--- _generatethumbnails.py
+++ _generatethumbnails.py
@@ -1,6 +1,7 @@
 import Image
 import ImageOps
 import yaml
+import time
 import os
 
 config = yaml.load(open("_serverconfig.yml", "r"))
@@ -9,6 +10,7 @@ dropbox_photos_path = os.path.join(config['dayonepath'], 'photos/')
 
 large_path = "_site/gfx/dayone_large"
 thumb_path = "_site/gfx/dayone_thumb"
+last_generated_path = ".lastgen"
 
 if not os.path.exists(large_path):
   os.makedirs(large_path)
@@ -17,9 +19,15 @@ if not os.path.exists(thumb_path):
 
 print "Generating thumbnails..."
 
+if os.path.isfile(last_generated_path):
+  lastgentime = os.path.getmtime(last_generated_path)
+else:
+  lastgentime = 0
+
 for photo_path in os.listdir(dropbox_photos_path):
   full_photo_path = os.path.join(dropbox_photos_path, photo_path)
-  if os.path.isfile(full_photo_path):
+  if os.path.isfile(full_photo_path) and os.path.getmtime(full_photo_path) > lastgentime:
+    print full_photo_path
     img = Image.open(full_photo_path)
 
     thumb = ImageOps.fit(img, (100,100), Image.ANTIALIAS)
@@ -31,5 +39,9 @@ for photo_path in os.listdir(dropbox_photos_path):
     else:
       img.thumbnail((938,938), Image.ANTIALIAS)
       img.save(os.path.join(large_path, photo_path))
+
+# Touch the lastmodified file
+with open(last_generated_path, 'a'):
+  os.utime(last_generated_path, None)
 
 print "Done."
