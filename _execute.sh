ef9a02b65100ac5dd74a6604baa94e3be4977cd2
Jeff Verkoeyen <jverkoey@gmail.com>
2013-12-02 19:52:37 -0500

    Better post rendering.

--- _execute.sh
+++ _execute.sh
@@ -1,1 +1,10 @@
-pygmentize -S default -f html > css/pygments.css;jekyll serve --watch --trace
+jekyll build
+cd _site
+pygmentize -S default -f html > ../css/pygments.css
+ln -s /Users/featherless/ServerDropbox/Dropbox/Apps/Day\ One/Journal.dayone/photos /Users/featherless/www/blog/_site/gfx/dayone
+cd ..
+python _generatethumbnails.py
+ln -s /Users/featherless/www/blog/.thumbs/dayone_large /Users/featherless/www/blog/_site/gfx/dayone_large
+ln -s /Users/featherless/www/blog/.thumbs/dayone_thumbs /Users/featherless/www/blog/_site/gfx/dayone_thumbs
+cd _site
+statify .
