3a59f25e309a0b32f7717dba305d9260366d1a9b
Jeff Verkoeyen <jverkoey@gmail.com>
2014-01-14 22:20:20 -0600

    Calculate totals.

--- _moneysfeatherless/moneys.rb
+++ _moneysfeatherless/moneys.rb
@@ -5,7 +5,33 @@ module Moneys
   class FeatherlessProcessor < Moneys::Processor
 
     def process_entry(entry)
+      currencyTotals = Hash.new
+
+      entry['portions'].each do |portion|
+        didPay = portion['didPay'].to_f
+        currency = portion['foreignCurrency']
+        if didPay > 0 and not currency.nil?
+          currencyTotals[currency] ||= 0
+          currencyTotals[currency] = currencyTotals[currency] + didPay
+        end
+      end
       
+      prettystr = Array.new
+      currencyTotals.each do |key,value|
+        formatted = '%.2f' % value
+        formatted.slice! ".00"
+        prettystr.concat([formatted + " " + key])
+      end
+
+      entry['total'] = prettystr * " + "
+    end
+    
+    def process_post(post)
+      if post.data.has_key?('moneys') then
+        moneys = post.data['moneys']
+        
+        
+      end
     end
 
   end
