c9085433c74ebce456dc0ebddf2c5a55a599dc57
Jeff Verkoeyen <jverkoey@gmail.com>
2014-01-14 21:12:01 -0600

    Shorter local time.

--- _plugins/localtime.rb
+++ _plugins/localtime.rb
@@ -7,6 +7,11 @@ module LocalTimeFilter
     return tz.utc_to_local(Time.parse(dayone['creation_date'].sub("+00:00", ''))).strftime('%A, %B %d, %Y %H:%M')
   end
 
+  def local_time_short(dayone)
+    tz = TZInfo::Timezone.get(dayone['time_zone'])
+    return tz.utc_to_local(Time.parse(dayone['creation_date'].sub("+00:00", ''))).strftime('%B %d, %Y')
+  end
+
 end
 
 Liquid::Template.register_filter(LocalTimeFilter)
