66960354c52b743382c2ceb9a812afa42f42e736
Jeff Verkoeyen <jverkoey@gmail.com>
2014-03-16 23:25:16 -1000

    Add geomapid.

--- _execute.sh
+++ _execute.sh
@@ -1,11 +1,8 @@
 jekyll build --trace
 cd _site
 pygmentize -S default -f html > ../css/pygments.css
-ln -s /Users/featherless/ServerDropbox/Dropbox/Apps/Day\ One/Journal.dayone/photos /Users/featherless/www/blog/_site/gfx/dayone
 cd ..
 python _generatethumbnails.py
-ln -s /Users/featherless/www/blog/.thumbs/dayone_large /Users/featherless/www/blog/_site/gfx/dayone_large
-ln -s /Users/featherless/www/blog/.thumbs/dayone_thumb /Users/featherless/www/blog/_site/gfx/dayone_thumb
 cd _site
 statify .
 
