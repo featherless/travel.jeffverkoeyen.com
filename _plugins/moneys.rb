e39736fd0897f31f935a77e3ea7f5f3526ba3a0a
Jeff Verkoeyen <jverkoey@gmail.com>
2014-07-25 00:15:29 +0700

    Publish and Taxes.

--- _plugins/moneys.rb
+++ _plugins/moneys.rb
@@ -4,8 +4,8 @@ require File.join(File.dirname(__FILE__), '..', '_moneysfeatherless/moneys.rb')
 module Moneys
   class Generator < Jekyll::Generator
     def generate(site)
-      processor = FeatherlessProcessor.new
-      processor.attach_moneys_to_site(site)
+      #processor = FeatherlessProcessor.new
+      #processor.attach_moneys_to_site(site)
     end
   end
 end
